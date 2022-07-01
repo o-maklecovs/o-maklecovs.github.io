ba8ef185dcde5973e60797f9ed28237aed1eda44
o-maklecovs <p2528743@my365.dmu.ac.uk>
2022-07-01 19:35:56 +0100

    Moved all content to a single page, removed spinning cube, changed colours and added a license file

--- js/nav.js
+++
@@ -1,5 +0,0 @@
-const menuToggler = document.querySelector('#menu-toggler');
-menuToggler.addEventListener('click', () => {
-  const navMenu = document.querySelector('#nav-menu');
-  navMenu.classList.toggle('open');
-});
